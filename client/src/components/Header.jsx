ae96e8cc5beae50927439f17e354f882b1b8fc33
Tomas Nader <tomi.nader@gmail.com>
2024-11-15 20:39:53 -0600

    Implement route back to homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Container, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Select, MenuItem, Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
+    const homepage = useNavigate();
     return (
         <AppBar color='transparent' position='static'>
             <Container>
                 <Toolbar>
-                    <Typography sx={{ fontFamily: "Montserrat, sans-serif" }}>
-                        Gemini
-                    </Typography>
+                    <Box sx={{ display: "flex", alignItems: "center", width: "100%" }}>
+                        <Typography onClick={() => homepage('/')}
+                            sx={{
+                                fontFamily: "Montserrat, sans-serif",
+                                fontWeight: "bold",
+                                color: "gold",
+                                flex: 1,
+                                cursor: "pointer",
+                            }}
+                        >
+                            Gemini
+                        </Typography>
+                        <Select
+                            variant="outlined"
+                            style={{
+                                width: 100,
+                                height: 40
+                            }}
+                        >
+                            <MenuItem value={"USD"}>USD</MenuItem>
+                            <MenuItem value={"ARS"}>ARS</MenuItem>
+                        </Select>
+                    </Box>
                 </Toolbar>
             </Container>
         </AppBar>
