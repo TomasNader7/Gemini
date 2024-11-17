3b916d746d4be5597c4b4f2385da708568e93e49
Tomas Nader <tomi.nader@gmail.com>
2024-11-17 13:04:11 -0600

    Created coins table to dispaly all coins

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
+import CoinsTable from '../components/CoinsTable';
 
 const Homepage = () => {
     return (
         <>
-            <Banner>
-            </Banner>
+            <Banner />
+            <CoinsTable />
         </>
-    )
+    );
 };
 
 export default Homepage;
